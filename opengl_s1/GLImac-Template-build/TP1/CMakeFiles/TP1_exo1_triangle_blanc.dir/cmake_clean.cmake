0fb0e543cbfe3c836c883f70e5dc09c3dc5c5766
DUSSOUCHAUD LUCAS <ldussouc@localhost.localdomain>
2016-10-05 17:49:48 +0200

    build template build template glimac ldussouc added

---
+++ opengl_s1/GLImac-Template-build/TP1/CMakeFiles/TP1_exo1_triangle_blanc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TP1_exo1_triangle_blanc.dir/exo1_triangle_blanc.cpp.o"
+  "TP1_exo1_triangle_blanc.pdb"
+  "TP1_exo1_triangle_blanc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TP1_exo1_triangle_blanc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
